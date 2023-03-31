8739420eb087cd6b97fc7698ce2fb83546c13ffd
Alex Bahn <AanderBahn@gmail.com>
2023-03-31 17:04:40 -0500

    Update script.js
    D.R.Y.ed up code by getting rid of repeat code

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 'use strict';
 
-/*
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'The Right Damn Number';
-document.querySelector('.score').textContent = 'We finally going to score';
-document.querySelector('.highscore').textContent = 'High Roller!';
-document.querySelector('.guess').value = 47;
-console.log(document.querySelector('.guess').value);*/
+
 
 //Get Secret number for game. Set limit to 47.
 var sNumber = Math.trunc(Math.random() * 47) + 1;
@@ -24,8 +18,17 @@ const displayMessage = function (message) {
 	document.querySelector('.message').textContent = message;
 }
 
+//FUNction to change the number display size
+const valueSize = function (size) {
+	document.querySelector('.number').style.width = size;
+}
+
+const changeBGColor = function (color) {
+	document.querySelector('body').style.backgroundColor = color;
+}
+
 //TESTING display SNumber. Delete after done testing.
-document.querySelector('.number').textContent = sNumber;
+//document.querySelector('.number').textContent = sNumber;
 
 
 
@@ -39,19 +42,16 @@ document.querySelector('.check').addEventListener('click', function(){
 	//for situion 0: no input
 	//if no value for input
 	if (!guessimate) {
-		//look through the DOM for Message. Change content to new message
-		//document.querySelector('.message').textContent = "No Number selected FOOL!";
 		displayMessage("No Number selected FOOL!");
 
 
 	  //if the input is the exace same
 	} else if (guessimate === sNumber) {
-		//document.querySelector('.message').textContent = "Congrads! You got it!";
 		displayMessage("Congrads! You got it!");
 		//Change Background color
-		document.querySelector('body').style.backgroundColor = '#60b347';
+		changeBGColor('#60b347');
 		//Change the width of number display
-		document.querySelector('.number').style.width = '30rem';
+		valueSize('30rem')
 
 		//Check to see if the current score is higher then last highscore
 		if (score > higherScore) {
@@ -63,42 +63,15 @@ document.querySelector('.check').addEventListener('click', function(){
 	} else if (guessimate !== sNumber) {
 		if (score > 0) {
 		//send the message of too high and lower score
-		//document.querySelector('.message').textContent = guessimate > sNumber ? "Too High" : "Too Low";
 		displayMessage(guessimate > sNumber ? "Too High" : "Too Low");
 		score--;
 		document.querySelector('.score').textContent = score;
 	    } else {
-	    	//document.querySelector('.message').textContent = "You Loose!"
+	    	//If score reaches 0
 	    	displayMessage("You Loose!");
 	    }//en
 	} 
 
-
-
-	//if the input is larger 
-	/*else if (guessimate > sNumber) {
-		//Score counter to work if above 0
-		if (score > 0) {
-		//send the message of too high and lower score
-		document.querySelector('.message').textContent = "Too High";
-		score--;
-		document.querySelector('.score').textContent = score;
-	    } else {
-	    	document.querySelector('.message').textContent = "You Loose!"
-	    }//end if inside of larger guess
-
-	  //if the input is smaller 
-	} else if (guessimate < sNumber) {
-		//Score counter to work if above 0
-		if (score > 0) {
-		//send message of too low and lower score
-		document.querySelector('.message').textContent = "Too Low";
-		score--;
-		document.querySelector('.score').textContent = score; 
-	   } else {
-	   	document.querySelector('.message').textContent = "You Loose!"
-	   }//End if smaller guess
-	}*/
 });
 
 
@@ -116,9 +89,9 @@ document.querySelector('.again').addEventListener('click', function(){
     //Reset the input
     document.querySelector('.guess').value = "";
 	//Reset Background color
-    document.querySelector('body').style.backgroundColor = '#222';
+    changeBGColor('#222');
     //Reset Width of display
-    document.querySelector('.number').style.width = '15rem';
+    valueSize('15rem')
 });
 
 
